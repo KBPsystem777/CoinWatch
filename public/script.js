a6cf02cdcef8408f83f4064f8523cdf700860228
Paunon <koleen.b.paunon@accenture.com>
2018-04-18 07:54:23 +0800

    Improved onload and numbers display

--- public/script.js
+++ public/script.js
@@ -1,25 +1,31 @@
-
+//@KBPsystem777 4-18-2018
 
 
 const allRates = document.querySelector('#getRates');
 
-let endPoint = "https://api.coinmarketcap.com/v1/ticker/";
+let endPoint = "https://api.coinmarketcap.com/v1/ticker/?convert=PHP";
 
-allRates.addEventListener('click', getAllRates);
+//allRates.addEventListener('click', getAllRates);
 
+
+//window.onload(getAllRates);
 
 const getAllRates = () => {
     console.log('Were getting it...');
     fetch(endPoint).then(response => response.json())
     .then(data => {
-        console.log(data);
+        //console.log(data);
         data.forEach(function(item,i){
             let loc = document.getElementById('output');
-            //let price = (Math.round(item.price_usd * 100)/100);
+            let price = (Math.round(item.price_php * 100)/100);
+            let newPrice = price.toLocaleString();
+
+
+
             let li = document.createElement('li');
             let span = document.createElement('span');
     
-            span.innerHTML = `${item.name}, ${item.symbol}, ${item.price_usd}`;
+            span.innerHTML = `${item.name}, Symbol: ${item.symbol}, PHP ${newPrice}`;
     
             li.appendChild(span);
             loc.appendChild(li);
@@ -29,3 +35,6 @@ const getAllRates = () => {
         console.log(JSON.stringify(error))
     })
 }
+
+window.onload = getAllRates();
+console.log('boom');
