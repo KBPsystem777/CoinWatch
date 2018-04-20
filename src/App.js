cd2b7e526774fba407d57dd12d33557c1531ce24
KBPsystem777 <koleen.bp@outlook.com>
2018-04-21 04:51:42 +0800

    Added Api fetch function

--- src/App.js
+++ src/App.js
@@ -1,45 +1,89 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+  import React, { Component } from 'react';
+  import logo from './logo.svg';
+  import './App.css';
 
 
 
-const SearchBar = () => {
-  return <input type="search"></input>
-}
-
-const SearchButton = () => {
-  return <button>Search</button>
-}
+  //API endpoint
+  const endPoint = 'https://api.coinmarketcap.com/v1/ticker/?limit=20';
 
 
-class SearchNav extends React.Component {
-  render() {
-    return(
-      <div>
-        <SearchBar />
-        <SearchButton />
-      </div>
-    );
+  class Top20Coins extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        id: "bitcoin",
+        rank: ""
+        //rank: ""
+      }
+    }
+    
+    componentDidMount() {
+      this.getTop20(this.state.id);
+    }
+
+
+    getTop20(id) {
+      fetch(endPoint)
+        .then(response => response.json())
+        .then(json => {
+          Top20Coins.setState({
+            rank: json.id
+          })
+        });
+    }
+
+    //description: json.weather[0].description
+
+
+    render() {
+      return(
+        <div>
+          <button onClick={this.getTop20}>Top 20 Coins</button>
+        </div>
+      );
+    }
   }
-}
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">CoinWatch App</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <SearchNav />
-      </div>
-    );
+
+  const SearchBar = () => {
+    return <input type="search"></input>
   }
-}
 
-export default App;
+  const SearchButton = () => {
+    return <button>Search</button>
+  }
+
+
+  class SearchNav extends React.Component {
+    render() {
+      return(
+        <div>
+          <SearchBar />
+          <SearchButton />
+        </div>
+      );
+    }
+  }
+
+
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">CoinWatch App</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <SearchNav />
+          <Top20Coins />
+        </div>  
+      );
+    }
+  }
+
+  export default App;
