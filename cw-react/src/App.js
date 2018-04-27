0edda72c2747a849805e937db9a46f6ba77cd8d0
KBPsystem777 <koleen.bp@outlook.com>
2018-04-28 03:37:49 +0800

    Removed react logo header

--- cw-react/src/App.js
+++ cw-react/src/App.js
@@ -1,80 +1,77 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ParticleBG from './Particles';
 
+//API endpoint
+const endPoint = 'https://api.coinmarketcap.com/v1/ticker/?limit=20';
 
-  //API endpoint
-  const endPoint = 'https://api.coinmarketcap.com/v1/ticker/?limit=20';
-
-  class Top20Coins extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        data: []
-      }
+class Top20Coins extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
     }
+  }
     
-    componentDidMount() {
-      this.getCoins(this.state.data);
-    }
-
-    getCoins(data) {
-      fetch(endPoint)
-        .then(response => response.json())
-        .then(json => {
-          //console.log(json);
-          this.setState({ json })
-          console.log(json);
-        });
-    }
-    render() {
-      return(
-        <div>
-          <p></p>
-        </div>
-      );
-    }
+  componentDidMount() {
+    this.getCoins(this.state.data);
   }
 
-
-
-  const SearchBar = () => {
-    return <input type="search"></input>
+  getCoins(data) {
+    fetch(endPoint)
+      .then(response => response.json())
+      .then(json => {
+        //console.log(json);
+        this.setState({ json })
+        console.log(json);
+      });
   }
-
-  const SearchButton = () => {
-    return <button>Search</button>
+  render() {
+    return(
+      <div>
+        <p></p>
+      </div>
+    );
   }
+}
 
-  class SearchNav extends React.Component {
-    render() {
-      return(
-        <div>
-          <SearchBar />
-          <SearchButton />
-        </div>
-      );
-    }
+
+
+const SearchBar = () => {
+  return <input type="search"></input>
+}
+
+const SearchButton = () => {
+  return <button>Search</button>
+}
+
+class SearchNav extends React.Component {
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <SearchButton />
+      </div>
+    );
   }
+}
 
-  class App extends Component {
-    render() {
-      return (
-        <div className="App">
-          <ParticleBG id="particles">
-            <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">CoinWatch App</h1>
-          </ParticleBG>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          <SearchNav />
-          <Top20Coins />
-        </div>  
-      );
-    }
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ParticleBG id="particles"/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <SearchNav />
+        <Top20Coins />
+      </div>  
+    );
   }
-
-  export default App;
+}
+export default App;
